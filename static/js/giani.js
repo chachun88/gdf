7c0701aec2b567d035c053c2eb8f8394ca6ece35
Ricardo Silva <ricardo@loadingplay.com>
2014-09-16 11:04:52 -0300

    temlates

--- static/js/giani.js
+++ static/js/giani.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
 
 	if(typeof(Storage) !== "undefined") {
 		if(!localStorage.guess_id){
-	    	$.ajax({
-	    		url: '/user/save-guess',
-	    		success: function(html){
-	    			if(html!="error"){
-	    				localStorage.guess_id = html;
-	    			}
-	    		}
-	    	});
-	    } else {
-	    	GetCartByUserId(localStorage.guess_id);
-	    }
+			$.ajax({
+				url: '/user/save-guess',
+				success: function(html){
+					if(html!="error"){
+						localStorage.guess_id = html;
+					}
+				}
+			});
+		} else {
+			GetCartByUserId(localStorage.guess_id);
+		}
 	}
 
 
