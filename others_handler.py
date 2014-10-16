f45acef97c1c58d6b4fc4bc1c55921ff78dc38e1
Yi Chun <yichun212@gmail.com>
2014-10-16 18:22:44 -0300

    remove simulation of success page

--- others_handler.py
+++ others_handler.py
@@ -311,10 +311,10 @@ class ExitoHandler(BaseHandler):
     # TBK_ORDEN_COMPRA:133
 
 
-    def get(self):
+    def post(self):
 
-        TBK_ID_SESION = self.get_argument("TBK_ID_SESION","20141015235139")
-        TBK_ORDEN_COMPRA = self.get_argument("TBK_ORDEN_COMPRA","133")
+        TBK_ID_SESION = self.get_argument("TBK_ID_SESION","")
+        TBK_ORDEN_COMPRA = self.get_argument("TBK_ORDEN_COMPRA","")
 
         myPath = "/var/www/giani.ondev/webpay/MAC01Normal{}.txt".format(TBK_ID_SESION)
         pathSubmit = "http://giani.ondev.today"
