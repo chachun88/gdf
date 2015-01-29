a7f23b140d1b65f61fd025a5624afe6f3a6e448c
Yi Chun <yichun212@gmail.com>
2015-01-29 19:29:51 -0300

    success

--- static/js/giani.login.js
+++ static/js/giani.login.js
@@ -145,7 +145,7 @@ $(document).ready(function(){
 
 				console.info(rtn_pair);
 
-				if (rtn_pair["success"]) 
+				if (rtn_pair["success"]!=undefined) 
 				{
 					window.location.href = rtn_pair["success"];
 				}
