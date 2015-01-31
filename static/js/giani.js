81a588d45b60662e8429a5ed9c47ee36ff4f6a94
Yi Chun <yichun212@gmail.com>
2015-01-30 23:18:45 -0300

    no message

--- static/js/giani.js
+++ static/js/giani.js
@@ -100,7 +100,7 @@ $(document).ready(function(){
 			}, 1000);
 		} else {
 			$("body,html").animate({
-				scrollTop: $(".footer").offset().top
+				scrollTop: $("#ajax_productos").offset().top
 			}, 1000);
 
 		}
