33b7b0c21601225fafa6f05af1c3f1be5dac81b7
Yi Chun <yichun212@gmail.com>
2015-01-30 22:20:18 -0300

    animate only when is in desktop

--- static/js/detalle-producto.js
+++ static/js/detalle-producto.js
@@ -17,8 +17,14 @@ $(document).ready(function(){
 				} else {
 					GetCartByUserId(window.localStorage.getItem("user_id"));
 					//fancyAlert("Producto ha sido a\xF1adido al carro");
-					$('html,body').animate({ scrollTop: 0 }, 'slow');
-					$(".carritoproductos").slideDown();
+					$("#icono-carro-movil").addClass("parpadea");
+
+					if($(window).width()>460){
+						$('html,body').animate({ scrollTop: 0 }, 'slow');
+						if($(".carritoproductos").css("display")=="none"){
+							$(".carritoproductos").slideDown();
+						}
+					}
 				}
 			}
 		})
