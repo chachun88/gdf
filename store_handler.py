3f1708965827d3888631f96e020eb8eec7887422
Yi Chun <yichun212@gmail.com>
2015-02-02 13:50:03 -0300

    detect lo nuevo tag

--- store_handler.py
+++ store_handler.py
@@ -48,9 +48,9 @@ class IndexHandler(BaseHandler):
 			tags = tags_visibles["success"]
 
 		if ajax == 0:
-			self.render("store/index.html",data=lista,items=items,page=page,tags=tags)
+			self.render("store/index.html",data=lista,items=items,page=page,tags=tags,tags_arr=None)
 		else:
-			self.render("store/ajax_productos.html",data=lista,items=items,page=page,tags=tags)
+			self.render("store/ajax_productos.html",data=lista,items=items,page=page,tags=tags,tags_arr=None)
 
 	def post(self):
 
@@ -243,9 +243,9 @@ class GetProductsByTagsHandler(BaseHandler):
 
 		if "success" in res:
 			if ajax == 0:
-				self.render("store/index.html",data=res["success"],items=items,page=page,tags=tags)
+				self.render("store/index.html",data=res["success"],items=items,page=page,tags=tags,tags_arr=tags_arr)
 			else:
-				self.render("store/ajax_productos.html",data=res["success"],items=items,page=page,tags=tags)
+				self.render("store/ajax_productos.html",data=res["success"],items=items,page=page,tags=tags,tags_arr=None)
 				#self.write(json_util.dumps({"html":self.render_string("store/ajax_productos.html",data=res["success"],url_bodega=url_bodega,money_format=self.money_format),"items":items,"page":page}))
 		else:
 			self.render("beauty_error.html",message=res["error"])
