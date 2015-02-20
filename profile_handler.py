9f9ac1836d23de11fb2ae9ab99e2d4d823b9688b
unknown <chienhung.lin@usach.cl>
2015-02-20 16:04:04 -0300

    Implementado el cambio de contraseña, por el momento los mensaje de notificacion se manda por medio de alert

--- profile_handler.py
+++ profile_handler.py
@@ -11,11 +11,12 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import hashlib
 
 from tornado.options import define, options
 
 from basehandler import BaseHandler
-
+from model.user import User
 
 class ProfileHandler(BaseHandler):
 
@@ -25,4 +26,27 @@ class ProfileHandler(BaseHandler):
 class ChangePassHandler(BaseHandler):
 
     def post(self):
-        self.render("profile/index.html")
+
+        oldpass = self.get_argument("oldpass","")
+        newpass = self.get_argument("newpass","")
+        confirmpass = self.get_argument("confirmpass","")
+
+        if self.current_user:
+            user_id = self.current_user["id"]
+            usuario = User().InitById(user_id)
+
+            m = hashlib.md5()
+            m.update(oldpass)
+            password = m.hexdigest()
+
+            if password == usuario["password"]:
+                if newpass == confirmpass:
+                    m = hashlib.md5()
+                    m.update(newpass)
+                    password = m.hexdigest()
+                    User().ChangePassword(user_id, password)
+                    self.write("exito")
+                else:
+                    self.write("claves ingresado no coinciden")
+            else:
+                self.write("clave incorrecta")
--- static/js/profile/profile.js
+++ static/js/profile/profile.js
@@ -11,4 +11,22 @@ $(document).ready(function(){
         $(".campo-contrasena").hide();
         $(".btn-cambiar-contrasena").show();
     });
+
+    $(".btn-guardar-contrasena").click(function(evt){
+
+        evt.preventDefault();
+        var data = $("form.cambiar-contrasena").serialize();
+
+        $.ajax({
+            url: "/profile/change_pass",
+            type: "post",
+            data: data,
+            success: function(html){
+                alert(html);
+            }
+        });
+
+    });
+
+
 });
