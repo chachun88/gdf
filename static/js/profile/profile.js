3fb285da0baf96dc90ba757ac87da9c06e373e60
unknown <chienhung.lin@usach.cl>
2015-02-23 14:23:42 -0300

    -Renombrar la clase de un elemento en el js -Implementado editar contacto, carga los datos del contacto seleccionado -Implementado cancelar editar contacto, -Implementacion en proceso del guardar cambio en contacto -Reordenar el html -Incorporar el formulario de contacto en la tabla (esta queda oculta hasta que opriman el boton) -borrar el comentario que hay debajo

--- static/js/profile/profile.js
+++ static/js/profile/profile.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     $(".formulario-contrasena").hide();
-    $(".formulario-direccion").hide();
+    $(".formulario-contacto").hide();
 
     $(".btn-cambiar-contrasena").click(function(){
         $(this).hide();
@@ -11,11 +11,6 @@ $(document).ready(function(){
     $(".btn-cancelar-cambio-contrasena").click(function(){
         $(".formulario-contrasena").hide();
         $(".btn-cambiar-contrasena").show();
-    });
-
-    $(".btn-editar-contacto").click(function(){
-        var id_contacto = $(this).parent().parent().attr("id-contacto");
-        alert("este es el id del contacto " + id_contacto);
     });
 
     $(".btn-guardar-contrasena").click(function(evt){
@@ -34,4 +29,43 @@ $(document).ready(function(){
 
     });
 
+    $(".btn-editar-contacto").click(function(){
+        var id = $(this).parent().parent().attr("id-contacto");
+        var name = $(this).parent().parent().attr("name");
+        var address = $(this).parent().parent().attr("address");
+        var town = $(this).parent().parent().attr("town");
+        var city = $(this).parent().parent().attr("city");
+        var zipcode = $(this).parent().parent().attr("zipcode");
+        var telephone = $(this).parent().parent().attr("telephone");
+        
+        $(".formulario-contacto #name").val(name);
+        $(".formulario-contacto #address").val(address);
+        $(".formulario-contacto #town").val(town);
+        $(".formulario-contacto #city").val(city);
+        $(".formulario-contacto #zip-code").val(zipcode);
+        $(".formulario-contacto #telephone").val(telephone);
+        $(".contactos").hide();
+        $(".formulario-contacto").show();
+    });
+
+    $(".btn-cancelar-editar-contacto").click(function(){
+        $(".formulario-contacto").hide();
+        $(".contactos").show();
+    });
+
+    $(".btn-guardar-contacto").click(function(evt){
+
+        evt.preventDefault();
+        var data = $("form.editar-contacto").serialize();
+
+        $.ajax({
+            url: "/profile/edit_contact",
+            type: "post",
+            data: data,
+            success: function(html){
+                alert(html);
+            }
+        });
+
+    });
 });
