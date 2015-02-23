b1e01cd5d6fdf92be80cee2c865ba5eb19c3cf2d
unknown <chienhung.lin@usach.cl>
2015-02-23 19:14:08 -0300

    Actualizar los datos de la lista si el cambio fue exitoso

--- static/js/profile/profile.js
+++ static/js/profile/profile.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
     $(".btn-guardar-contrasena").click(function(evt){
 
         evt.preventDefault();
+
         var data = $("form.cambiar-contrasena").serialize();
 
         $.ajax({
@@ -45,6 +46,7 @@ $(document).ready(function(){
         $(".formulario-contacto #city").val(city);
         $(".formulario-contacto #zip-code").val(zipcode);
         $(".formulario-contacto #telephone").val(telephone);
+        
         $(".contactos").hide();
         $(".formulario-contacto").show();
     });
@@ -57,16 +59,43 @@ $(document).ready(function(){
     $(".btn-guardar-contacto").click(function(evt){
 
         evt.preventDefault();
+
+        var id = $(".formulario-contacto #id-contacto").val();
+        var name = $(".formulario-contacto #name").val();
+        var address = $(".formulario-contacto #address").val();
+        var town = $(".formulario-contacto #town").val();
+        var city = $(".formulario-contacto #city").val();
+        var zipcode = $(".formulario-contacto #zip-code").val();
+        var telephone = $(".formulario-contacto #telephone").val();
+
+        var exito = false;
+
         var data = $("form.editar-contacto").serialize();
 
         $.ajax({
             url: "/profile/edit_contact",
             type: "post",
             data: data,
+            async: false,
             success: function(html){
-                alert(html);
+                if(html == "El cambio fue exitoso"){
+                    exito = true;
+                }else{
+                    alert(html);
+                }
             }
         });
 
+        if(exito){
+            $("#name-" + id).html(name);
+            $("#address-" + id).html(address);
+            $("#town-" + id).html(town);
+            $("#city-" + id).html(city);
+            $("#zipcode-" + id).html(zipcode);
+            $("#telephone-" + id).html(telephone);
+
+            $(".formulario-contacto").hide();
+            $(".contactos").show();
+        }
     });
 });
