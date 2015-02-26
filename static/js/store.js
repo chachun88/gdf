510667eff616e7536306f478f81d1e30dc11ca51
unknown <chienhung.lin@usach.cl>
2015-02-26 14:43:41 -0300

    convertir tab en space

--- static/js/store.js
+++ static/js/store.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
 
-	console.info($("#total-items").val());
+    console.info($("#total-items").val());
 
-	$(".paginador").pagination({
+    $(".paginador").pagination({
       items: $("#total-items").val(),
       itemsOnPage: 16,
       prevText: '<',
@@ -13,60 +13,60 @@ $(document).ready(function(){
       hrefTextSuffix: '#contenedor'
     });
 
-	var expanded = false;
-	var original_height = $("#menu-m ul").css('height')
-	$("#menu-m ul").css('height',0);
+    var expanded = false;
+    var original_height = $("#menu-m ul").css('height')
+    $("#menu-m ul").css('height',0);
 
-	//$("#menu-m ul").slideDown("fast");
+    //$("#menu-m ul").slideDown("fast");
 
-	$(".cover-material").click(function(event) {
+    $(".cover-material").click(function(event) {
 
-		event.stopPropagation();
+        event.stopPropagation();
 
-		// Slide down menu if hidden
-		if (!expanded) {
-			if($(window).width() <= 480){$("#filtrar-btn-menu").fadeIn("slow");}
-			$("#menu-m ul").animate({
-				"height": original_height
-			}, "slow");
-			expanded = true;
-		}
-		// Slide up menu if shown
-		else {
-			$("#filtrar-btn-menu").fadeOut("slow");
-			$("#menu-m ul").animate({
-				"height": 0
-			}, "slow");
-			expanded = false;
-		}
+        // Slide down menu if hidden
+        if (!expanded) {
+            if($(window).width() <= 480){$("#filtrar-btn-menu").fadeIn("slow");}
+            $("#menu-m ul").animate({
+                "height": original_height
+            }, "slow");
+            expanded = true;
+        }
+        // Slide up menu if shown
+        else {
+            $("#filtrar-btn-menu").fadeOut("slow");
+            $("#menu-m ul").animate({
+                "height": 0
+            }, "slow");
+            expanded = false;
+        }
 
-		
+        
 
-	});
+    });
 
-	$("#menu-m").click(function(event){
-		event.stopPropagation();
-	});
-	
-	$(document).click(function(){
-		if(expanded){
-			$("#filtrar-btn-menu").fadeOut("slow");
-			$("#menu-m ul").animate({
-				"height": 0
-			}, "slow");
-			expanded = false;	
-		}
-	});
-	$(".cerrar-filtrar-btn-menu").click(function(){
-		if(expanded){
-			$("#filtrar-btn-menu").fadeOut("fast");
-			$("#menu-m ul").animate({
-				"height": 0
-			}, "fast");
-			expanded = false;	
-		}
-	});
-	
-	
+    $("#menu-m").click(function(event){
+        event.stopPropagation();
+    });
+    
+    $(document).click(function(){
+        if(expanded){
+            $("#filtrar-btn-menu").fadeOut("slow");
+            $("#menu-m ul").animate({
+                "height": 0
+            }, "slow");
+            expanded = false;   
+        }
+    });
+    $(".cerrar-filtrar-btn-menu").click(function(){
+        if(expanded){
+            $("#filtrar-btn-menu").fadeOut("fast");
+            $("#menu-m ul").animate({
+                "height": 0
+            }, "fast");
+            expanded = false;   
+        }
+    });
+    
+    
 
 });
