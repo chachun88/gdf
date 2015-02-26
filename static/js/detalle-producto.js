efd93a8b3859de90c58b9cd09f83761bcdf8c2f9
unknown <chienhung.lin@usach.cl>
2015-02-26 19:07:12 -0300

    Implementado Google Analytics addImpresion en detalle producto

--- static/js/detalle-producto.js
+++ static/js/detalle-producto.js
@@ -97,5 +97,24 @@ $(document).ready(function(){
 
     $("ul.tabs-menu-li li:first a").addClass("active").trigger("click");
 
+    // Obtencion de datos para ga
+    var ga_id = $("select#size ").attr("sku");
+    var ga_name = $(".producto-nombre-1").html();
+    var ga_category = $(".ga-category").html();
+    var ga_variant = $(".ga-variant").html();
+    var ga_price = $(".ga-price").html();
+
+    ga('ec:addImpression', {
+        'id': ga_id,
+        'name': ga_name,
+        // 'list': 'Search Results',
+        'brand': 'Giani Da Firenze',
+        'category': ga_category,
+        'variant': ga_variant,
+        // 'position': 1,
+        'price': ga_price,
+    });
+
+    ga('send', 'pageview');
 });
 
