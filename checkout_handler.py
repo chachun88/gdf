4ea64c9d923e0a146a4708332feedd206259b5b5
Yi Chun <yichun212@gmail.com>
2015-04-09 20:53:10 -0300

    use price instead of sell_price

--- checkout_handler.py
+++ checkout_handler.py
@@ -510,7 +510,7 @@ class CheckoutSendHandler(BaseHandler):
                                 <td style="line-height: 2.5;margin-left: -1px;height: 30px;border-right: 1px;border-right-color: #d6d6d6; border-right-style: solid;border-bottom: 1px; border-bottom-style: solid;border-bottom-color: #d6d6d6;">$ {price}</td>
                                 <td style="line-height: 2.5;margin-left: -1px;height: 30px;border-right: 1px;border-right-color: #d6d6d6; border-right-style: solid;border-bottom: 1px; border-bottom-style: solid;border-bottom-color: #d6d6d6;">{subtotal}</td>
                             </tr>
-                        """.format(name=l["name"],size=l["size"],quantity=l["quantity"],color=l["color"],price=self.money_format(l["sell_price"]),subtotal=self.money_format(l["subtotal"]))
+                        """.format(name=l["name"],size=l["size"],quantity=l["quantity"],color=l["color"],price=self.money_format(l["price"]),subtotal=self.money_format(l["subtotal"]))
 
                     contact = Contact()
                     facturacion_response = contact.InitById(order.billing_id)
