6a28ab886b7fbe2aaf73eab05b89afc63d51b1ef
Yi Chun <yichun212@gmail.com>
2015-04-10 17:56:08 -0300

    comentar google analytics

--- static/js/ga-pageview-detalle-producto.js
+++ static/js/ga-pageview-detalle-producto.js
@@ -3,7 +3,7 @@
 $(document).ready(function()
 {
     // Obtencion de datos para ga
-    var ga_id = $("select#size ").attr("sku");
+/*    var ga_id = $("select#size ").attr("sku");
     var ga_name = $(".producto-nombre-1").html();
     var ga_category = $(".ga-category").html();
     var ga_variant = $(".ga-variant").html();
@@ -24,5 +24,5 @@ $(document).ready(function()
     ga('ec:setAction', 'detail');
 
     ga('send', 'pageview');
-
+*/
 });
