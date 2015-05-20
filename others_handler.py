cd6a4f2c5a6a0b4481560a2ec55122db6d5fc261
Yi Chun <yichun212@gmail.com>
2015-05-19 21:03:16 -0300

    para probar mail

--- others_handler.py
+++ others_handler.py
@@ -99,9 +99,9 @@ class TosHandler(BaseHandler):
 
 class PagoHandler(BaseHandler):
 
-    # def get(self):
+    def get(self):
 
-    #     self.render("pago.html")
+        self.render("pago.html")
 
     def post(self):
 
