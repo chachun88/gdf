48e84608ca8aa899241db024c1e71839e2ab1a5c
Yi Chun Lin <yi@loadingplay.com>
2015-06-12 19:40:37 -0300

    login antiguo

--- model/user.py
+++ model/user.py
@@ -748,14 +748,12 @@ class User(BaseModel):
             left join "Cellar" c on c.id = any(u.cellar_permissions) 
             where u.rut = %(rut)s and 
                 u.password = %(password)s and
-                u.status = %(status)s and
-                u.type_id = %(type)s
+                u.status = %(status)s
             group by u.id limit 1'''
         p = {
             "rut": username,
             "password": password,
-            "status": self.ACEPTADO,
-            "type": self.getUserTypeID(UserType.EMPRESA)
+            "status": self.ACEPTADO
         }
         try:
             # print curs.mogrify( q, p )
