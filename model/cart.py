f80603badce9a41de954c23f33a3f04129a958dc
Yi Chun Lin <yi@loadingplay.com>
2016-01-04 20:48:03 -0300

    move to logged

--- model/cart.py
+++ model/cart.py
@@ -386,10 +386,13 @@ class Cart(BaseModel):
 
     def MoveTempToLoggedUser(self, old_user_id, current_user_id):
 
+        old_cart = []
+        new_cart = []
+
         cur = self.connection.cursor(
             cursor_factory=psycopg2.extras.RealDictCursor)
 
-        query = '''update "Temp_Cart" set user_id = %(current_user_id)s where user_id = %(old_user_id)s'''
+        query = '''select * from "Temp_Cart" where user_id = %(current_user_id)s'''
         parameters = {
             "current_user_id": current_user_id,
             "old_user_id": old_user_id
@@ -397,13 +400,45 @@ class Cart(BaseModel):
 
         try:
             cur.execute(query, parameters)
+            new_cart = cur.fetchall()
             self.connection.commit()
-            return self.ShowSuccessMessage("Cart has been move to logged user")
         except Exception, e:
             return self.ShowError(str(e))
         finally:
             cur.close()
             self.connection.close()
+
+
+        for item in new_cart:
+
+            cur = self.connection.cursor(
+                cursor_factory=psycopg2.extras.RealDictCursor)
+
+            query = '''select id from "Temp_Cart" where user_id = %(old_user_id)s and product_id = %(product_id)s'''
+            parameters = {
+                "current_user_id": current_user_id,
+                "old_user_id": old_user_id,
+                "product_id": item["product_id"]
+            }
+
+            try:
+                cur.execute(query, parameters)
+                tc = cur.fetchone()
+                if tc is not None:
+                    q = '''update "Temp_Cart" set quantity = quantity + 1, subtotal = quantity * price where id = %(id)s'''
+                    p = {
+                        "id": tc["id"]
+                    }
+                    cur.execute(q, p)
+                else:
+                    q = '''update "Temp_Cart" set user_id = %(current_user_id)s where user_id = %(old_user_id)s and product_id = %(product_id)s'''
+                    cur.execute(q, parameters)
+                self.connection.commit()
+            except Exception, e:
+                pass
+            finally:
+                cur.close()
+                self.connection.close()
 
         # contact = Contact()
         # order = Order()
