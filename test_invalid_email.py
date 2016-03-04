9acd001f49482039fb95577cc0834a8da26fb56a
Yi Chun Lin <yi@loadingplay.com>
2016-03-04 16:29:37 -0300

    script update shipping price

--- test_invalid_email.py
+++ test_invalid_email.py
@@ -7,6 +7,7 @@ import psycopg2.extras
 import datetime
 import pytz
 from bson import json_util
+import xlrd
 
 
 # os.environ['TZ'] = 'Chile'
@@ -121,7 +122,112 @@ class SavePostOffices():
                     cur.close()
                     connection.close()
 
+
+class UpdatePrice():
+
+    def main(self):
+
+        doc = xlrd.open_workbook("chilexpress_2016.xlsx")
+
+        sheet = doc.sheet_by_index(0)
+
+        nrows = sheet.nrows
+        ncols = sheet.ncols
+        # print ncols
+        # self.write("{}".format(ncols))
+
+        # fila
+        for i in range(1, nrows):
+
+            city_id = None
+            price = 0
+            ciudad = ""
+
+            # columna
+            for j in range(ncols):
+
+                if j == 0:
+                    value = sheet.cell_value(i,j)
+                    ciudad = value
+
+                    connection = psycopg2.connect("host='localhost' dbname='giani' user='postgres' password='12345'")
+                    cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+                    try:
+                        query = '''
+                                select id from "City"
+                                where lower(name) = lower(%(name)s)
+                                '''
+                        parameters = {
+                            "name": value
+                        }
+                        cur.execute(query, parameters)
+                        city_id = cur.fetchone()["id"]
+                        connection.commit()
+                    except Exception,e:
+                        city_id = 0
+                        print str(e)
+                    finally:
+                        cur.close()
+                        connection.close()
+
+                    if city_id == 0:
+
+                        connection = psycopg2.connect("host='localhost' dbname='giani' user='postgres' password='12345'")
+                        cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+                        try:
+                            query = '''
+                                    insert into "City" (name)
+                                    values (lower(%(name)s))
+                                    returning id
+                                    '''
+                            parameters = {
+                                "name": value
+                            }
+                            cur.execute(query, parameters)
+                            city_id = cur.fetchone()["id"]
+                            connection.commit()
+                        except Exception,e:
+                            city_id = None
+                            # print str(e)
+                        finally:
+                            cur.close()
+                            connection.close()
+
+                elif j == 1:
+                    price = sheet.cell_value(i,j)
+
+            if city_id is not None and price > 0:
+
+                connection = psycopg2.connect("host='localhost' dbname='giani' user='postgres' password='12345'")
+                cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+                try:
+                    query = '''
+                            update "Shipping"
+                            set chilexpress_price = %(price)s,
+                            price = %(price)s
+                            where to_city_id = %(city_id)s
+                            '''
+                    parameters = {
+                        "city_id": city_id,
+                        "price": price
+                    }
+                    cur.execute(query, parameters)
+                    connection.commit()
+                except Exception,e:
+                    print str(e)
+                finally:
+                    cur.close()
+                    connection.close()
+
+
+
 # send_email = SendEmail()
 # send_email.main()
-savestate = SavePostOffices()
-savestate.main()
+# savestate = SavePostOffices()
+# savestate.main()
+
+c = UpdatePrice()
+c.main()
